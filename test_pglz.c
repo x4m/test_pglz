a97f63bf9ff07c383a8a7a6b98ed45a7bc546dfd
Andrey <amborodin@acm.org>
2019-05-16 17:57:40 +0500

    More slices tests

--- test_pglz.c
+++ test_pglz.c
@@ -45,10 +45,6 @@ pglz_decompress_hacked(const char *source, int32 slen, char *dest,
 						 int32 rawsize, bool check_complete);
 
 int32
-pglz_decompress_hacked4(const char *source, int32 slen, char *dest,
-						 int32 rawsize, bool check_complete);
-
-int32
 pglz_decompress_hacked8(const char *source, int32 slen, char *dest,
 						 int32 rawsize, bool check_complete);
 
@@ -56,12 +52,9 @@ int32
 pglz_decompress_hacked16(const char *source, int32 slen, char *dest,
 						 int32 rawsize, bool check_complete);
 
-int32
-pglz_decompress_hacked32(const char *source, int32 slen, char *dest,
-						 int32 rawsize, bool check_complete);
-
 
 double do_test(int compressor, int decompressor, int payload);
+double do_sliced_test(int compressor, int decompressor, int payload, int slice_size);
 
 compress_func compressors[] = {pglz_compress_vanilla};
 char *compressor_name[] = {"pglz_compress_vanilla"};
@@ -71,24 +64,20 @@ decompress_func decompressors[] =
 {
 	pglz_decompress_vanilla,
 	pglz_decompress_hacked,
-	pglz_decompress_hacked4,
 	pglz_decompress_hacked8,
 	pglz_decompress_hacked16,
-	pglz_decompress_hacked32,
 	pglz_decompress_vanilla,
 };
 char *decompressor_name[] = 
 {
 	"pglz_decompress_vanilla - warmup", /* do vanilla test at the beginning and at the end */
 	"pglz_decompress_hacked",
-	"pglz_decompress_hacked4",
 	"pglz_decompress_hacked8",
 	"pglz_decompress_hacked16",
-	"pglz_decompress_hacked32",
 	"pglz_decompress_vanilla",
 };
 
-int decompressors_count = 7;
+int decompressors_count = 5;
 
 char *payload_names[] =
 {
@@ -140,6 +129,51 @@ double do_test(int compressor, int decompressor, int payload)
 	return ((double)decopmression_end - decompression_begin) * (1000000000.0L / size) / CLOCKS_PER_SEC;
 }
 
+double do_sliced_test(int compressor, int decompressor, int payload, int slice_size)
+{
+	ereport(LOG,
+		(errmsg("Testing %dKb slicing payload %s\tcompressor %s\tdecompressor %s", slice_size / 1024,
+			payload_names[payload], compressor_name[compressor], decompressor_name[decompressor]),
+		errhidestmt(true)));
+	char *data = payloads[payload];
+	long size = payload_sizes[payload];
+	int slice_count = size / slice_size;
+	void **extracted_data = palloc(slice_count * sizeof(void*));
+	void **compressed = palloc(slice_count * sizeof(void*));
+	int *comp_size = palloc(slice_count * sizeof(int));
+	int i;
+	for (i = 0; i < slice_count; i++)
+	{
+		extracted_data[i] = palloc(slice_size * 2);
+		compressed[i] = palloc(slice_size * 2);
+	}
+
+	for (i = 0; i < slice_count; i++)
+		comp_size[i] = compressors[compressor](data + slice_size * i, slice_size, compressed[i], PGLZ_strategy_default);
+
+	clock_t decompression_begin = clock();
+	for (i = 0; i < slice_count; i++)
+	{
+		if (comp_size[i] == -1)
+			continue; /* no decompression */
+		int decompressed_size = decompressors[decompressor](compressed[i], comp_size[i], extracted_data[i], slice_size, false);
+		if (decompressed_size != slice_size)
+			elog(ERROR, "decompressed wrong size %d instead of %d, compressed size %d", decompressed_size, slice_size, comp_size[i]);
+	}
+	clock_t decopmression_end = clock();
+
+	for (i = 0; i< slice_count; i++)
+	{
+		pfree(extracted_data[i]);
+		pfree(compressed[i]);
+	}
+	pfree(extracted_data);
+	pfree(comp_size);
+	pfree(compressed);
+
+	return ((double)decopmression_end - decompression_begin) * (1000000000.0L / size) / CLOCKS_PER_SEC;
+}
+
 static void prepare_payloads()
 {
 	payloads = palloc(sizeof(void*) * payload_count);
@@ -180,6 +214,10 @@ Datum
 test_pglz(PG_FUNCTION_ARGS)
 {
 	double results[10][10];
+	double sliced_2kb_results[10][10];
+	double sliced_8kb_results[10][10];
+
+	double decompressor_results[10];
 	int iterations = 5;
 	int iteration;
 	int i,p;
@@ -189,10 +227,22 @@ test_pglz(PG_FUNCTION_ARGS)
 	for (p = 0; p < payload_count; p++)
 		for (i = 0; i < decompressors_count; i++)
 		{
+			decompressor_results[i] = 0;
 			results[p][i] = 0;
 			for (iteration = 0; iteration < iterations; iteration++)
 				results[p][i] += do_test(0, i, p);
 			results[p][i] /= iterations;
+
+			sliced_2kb_results[p][i] = 0;
+			for (iteration = 0; iteration < iterations; iteration++)
+				sliced_2kb_results[p][i] += do_sliced_test(0, i, p, 2048);
+			sliced_2kb_results[p][i] /= iterations;
+			results[p][i] /= iterations;
+
+			sliced_8kb_results[p][i] = 0;
+			for (iteration = 0; iteration < iterations; iteration++)
+				sliced_8kb_results[p][i] += do_sliced_test(0, i, p, 4096);
+			sliced_8kb_results[p][i] /= iterations;
 		}
 
 	Log_error_verbosity = PGERROR_TERSE;
@@ -203,8 +253,30 @@ test_pglz(PG_FUNCTION_ARGS)
 		for (i = 1; i < decompressors_count; i++)
 		{
 			ereport(NOTICE, (errmsg("Decompressor %s result %f", decompressor_name[i], results[p][i]), errhidestmt(true)));
+			decompressor_results[i] += results[p][i];
+		}
+
+		ereport(NOTICE, (errmsg("Payload %s sliced by 2Kb", payload_names[p]), errhidestmt(true)));
+		for (i = 1; i < decompressors_count; i++)
+		{
+			ereport(NOTICE, (errmsg("Decompressor %s result %f", decompressor_name[i], sliced_2kb_results[p][i]), errhidestmt(true)));
+			decompressor_results[i] += sliced_2kb_results[p][i];
+		}
+
+		ereport(NOTICE, (errmsg("Payload %s sliced by 8Kb", payload_names[p]), errhidestmt(true)));
+		for (i = 1; i < decompressors_count; i++)
+		{
+			ereport(NOTICE, (errmsg("Decompressor %s result %f", decompressor_name[i], sliced_8kb_results[p][i]), errhidestmt(true)));
+			decompressor_results[i] += sliced_8kb_results[p][i];
 		}
 	}
+
+	ereport(NOTICE, (errmsg("\n\nDecompressor score (summ of all times):"), errhidestmt(true)));
+	for (i = 1; i < decompressors_count; i++)
+	{
+		ereport(NOTICE, (errmsg("Decompressor %s result %f", decompressor_name[i], decompressor_results[i]), errhidestmt(true)));
+	}
+
 	Log_error_verbosity = old_verbosity;
 
 	PG_RETURN_VOID();
